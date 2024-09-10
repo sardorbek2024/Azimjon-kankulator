e94862c6c8012ecfb0e1cb21084e86eaeaec189a
sardorbek2024 <168269584+sardorbek2024@users.noreply.github.com>
2024-09-10 09:55:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+let number1 = +prompt("1-soni kiriniting")//3
+let daraja = +prompt("daraja kiriting")//5
+
+let natija = 1;  //125
+
+for(let i = 1;   i <= daraja; i++) {
+    natija = number1 * natija
+}
+
+alert(natija)
+
+
+
+
+
+
+
+
